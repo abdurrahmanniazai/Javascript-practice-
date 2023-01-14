2480498264b8f5197e31968f8c94d13375bbff90
abdurrahmanniazai <111975269+abdurrahmanniazai@users.noreply.github.com>
2023-01-14 21:36:57 +0500

    Add files via upload

---
+++ expressions1.js
@@ -0,0 +1,17 @@
+let a = prompt('your age please?');
+a = Number.parseInt(a); 
+
+if (a<0){
+  alert('This is an invalid age');
+}
+else if(a<9){
+  alert('you are a kid');
+}
+else if(a<18 && a>= 9) {
+  alert("you are a kid too");
+}
+else{
+  alert("this is an invalid age");
+}
+console.log("done")
+console.log("you can", (age<18? "not drive": "drive" ))
