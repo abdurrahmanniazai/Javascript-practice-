f0bf22280fbfc66a745bc2f46ef164a814f0f7f6
abdurrahmanniazai <111975269+abdurrahmanniazai@users.noreply.github.com>
2023-01-14 21:05:53 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let a = 45;
+let b = 4;
+
+console.log('a + b =', a+b)
+console.log('a-b=', a-b)
+
+console.log("a ** b=", a ** b)
+
+
+
+let comp1 = 6;
+let comp2 = 7;
+console.log('comp1 == comp2 is', comp1 == comp2)
+
+
+let x = 5;
+let y = 6;
+console.log(x<y && x ==5)
+console.log(x>y !!)
