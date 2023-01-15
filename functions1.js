483ceebb98a83a0bdbb884fa23d7f483540ac0b2
abdurrahmanniazai <111975269+abdurrahmanniazai@users.noreply.github.com>
2023-01-15 15:22:29 +0500

    Add files via upload

---
+++ functions1.js
@@ -0,0 +1,11 @@
+let a = 1;
+let b= 2;
+let c = 3;
+function oneplusAvg(x, y){
+  return 1 + (x + y)/2
+  
+}
+console.log('average of a and b is',  oneplusAvg(a,b))
+console.log('Done')
+console.log('Average of a and b',(a+b)/2)
+
