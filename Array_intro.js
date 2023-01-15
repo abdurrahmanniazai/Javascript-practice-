0d77bfaedc5958ec9291e655a25109cc515aa228
abdurrahmanniazai <111975269+abdurrahmanniazai@users.noreply.github.com>
2023-01-15 16:43:58 +0500

    Add files via upload

---
+++ Array_intro.js
@@ -0,0 +1,8 @@
+let marks_class_12 = [12,13,14, false, 'not present']
+console.log(marks_class_12)
+console.log(marks_class_12[1])
+
+//changing value inside an array
+marks_class_12[2] = 10
+console.log(marks_class_12)
+console.log(typeof marks_class_12)
