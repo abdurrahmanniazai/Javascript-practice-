b0e50d6ac640c860b00ded1385eb15b2ccf4dc6e
abdurrahmanniazai <111975269+abdurrahmanniazai@users.noreply.github.com>
2023-01-16 21:33:55 +0500

    Add files via upload

---
+++ matchesEtcJavascript/script.js
@@ -0,0 +1,6 @@
+let id1 = document.getElementById("id1")
+console.log(id1)
+console.log(id1.matches(".class"))
+console.log(id1.matches(".box")) 
+console.log(sp1.closest("#sp1"))
+console.log(sp1.closest("#sp1"))
